d149024218c4e81a8096e46753f8b6800944bed4
Arun Kumar <kumar05.962013@gmail.com>
2022-10-16 17:00:44 +0530

    get hello msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+// Creating a GQL Query by wrapping it as a `template string` in the gql function
+const GET_HELLO_MSG = gql`
+  query GetHelloMessage {
+    hello
+  }
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Passing the GQL query "GET_HELLO_MSG" to the useQuery hook.
+  const { loading, error, data } = useQuery(GET_HELLO_MSG);
+  // Above, observe: loading, error, and data properties being destructured from the useQuery hook
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return <h1>{data.hello}</h1>;
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,27 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      query GetHelloMessage {
-        hello
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
